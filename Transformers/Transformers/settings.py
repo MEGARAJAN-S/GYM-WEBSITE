9ca1604ff31ee376a83c5ac2e9f702d50036bbac
MEGARAJAN <63449890+MEGARAJAN-S@users.noreply.github.com>
2024-05-31 14:38:45 +0530

    Update settings.py

--- Transformers/Transformers/settings.py
+++ Transformers/Transformers/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["13.50.106.153"]
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/"static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
