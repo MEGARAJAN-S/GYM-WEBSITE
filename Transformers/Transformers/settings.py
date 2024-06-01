aa4842339351cf67515632ec038a49d0fd21ce90
MEGARAJAN <63449890+MEGARAJAN-S@users.noreply.github.com>
2024-06-01 23:22:58 +0530

    Update settings.py

--- Transformers/Transformers/settings.py
+++ Transformers/Transformers/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-arrxm-ifakdbiv=h)4t_e6+8@6!^@qhdp1&l2kzmkz6u&=-%-2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["43.204.218.30"]
+ALLOWED_HOSTS = [""]
 
 
 # Application definition
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/"static/"
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
